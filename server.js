c5a2e10e21ecc6198b02d42d84d96b85d027937a
Jordan Moss <jmoss636@gmail.com>
2018-03-30 20:18:21 +0000

    Added pagination

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express')
 const fetch = require('node-fetch')
 const {authenticate} = require('pixabay-api')
 const fs = require('fs')
-//const R = require('ramda')
 
-const AUTH_KEY = '4263028-86143d706a6598bf902aee3f6'
+const AUTH_KEY = process.env.AUTH_KEY
 const PORT = process.env.PORT || 8080
 const {searchImages} = authenticate(AUTH_KEY)
 const storeURL = require('path').resolve('./data/searches.json')
@@ -12,39 +11,41 @@ const SEARCH_STORE = JSON.parse(fs.readFileSync(storeURL))
 const app = express()
 
 const filterImages = e => makeImageObject(e.previewURL, e.webformatURL, e.tags, e.pageURL)
-const makeImageObject = (thumbnailUrl, webUrl, alt, pictureLocation) => {
-    console.log('Making new image object')
-    return new Object({
-        thumbnailUrl,
-        webUrl,
-        alt,
-        pictureLocation
-    })
-}
+const makeImageObject = (thumbnailUrl, webUrl, alt, pictureLocation) => {return new Object({thumbnailUrl, webUrl, alt, pictureLocation})}//I know I can use {{}} but I get weird bugs when I do, so it's staying like this
 
 const updateStorage = (query, arr) => {
     SEARCH_STORE[query] = arr
-    fs.writeFile(storeURL, SEARCH_STORE, 'utf-8')
+    fs.writeFile(storeURL, JSON.stringify(SEARCH_STORE), 'utf-8')
     return arr
 }
-        
+
+const getCachedResults = (query, offset) => {
+    const cachedData = SEARCH_STORE[query]
+    if(!cachedData) return false
+    return paginate(cachedData, offset)
+}
+
+const paginate = (cachedData, offset) => {
+    const upperLimit = offset, lowerLimit = upperLimit - 10
+    if(upperLimit > cachedData.length) return false
+    
+    return cachedData.slice(lowerLimit, upperLimit)
+}
+    
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
-app.get('/api/:query', (req,res) => {
-    const query = req.params.query, limit =10
-    const result= SEARCH_STORE[query]
-    console.log(Object.keys(SEARCH_STORE), query)
+app.get('/api/:query/:offset', (req,res) => {
+    const {query, offset} = req.params
+    const cachedData= getCachedResults(query, offset * 10)
     
-    if(result){
-        console.log('Cached result')
-      res.send(result)
+    if(cachedData){
+        res.send(cachedData)
     } else {
-        console.log('Searching. . .')
-     searchImages(query, {per_page: limit})
+        searchImages(query, {per_page: offset * 10})
         .then(r => r.hits.map(filterImages))
         .then(imageArray => updateStorage(query, imageArray))
-        .then(imageArray => res.send(imageArray))
+        .then(imageArray => res.send(paginate(imageArray, offset * 10)))
     }
 })
 
