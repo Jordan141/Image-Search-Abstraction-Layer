260fab039572eecfb8a79adcb0becfbd64d14567
Jordan Moss <jmoss636@gmail.com>
2018-04-01 22:53:56 +0000

    glitch export

--- server.js
+++ server.js
@@ -3,18 +3,27 @@ const fetch = require('node-fetch')
 const {authenticate} = require('pixabay-api')
 const fs = require('fs')
 
-const AUTH_KEY = process.env.AUTH_KEY
+const {AUTH_KEY} = require('./config.json')
 const PORT = process.env.PORT || 8080
 const {searchImages} = authenticate(AUTH_KEY)
 const storeURL = require('path').resolve('./data/searches.json')
-const SEARCH_STORE = JSON.parse(fs.readFileSync(storeURL))
+let SEARCH_STORE = JSON.parse(fs.readFileSync(storeURL))
 const app = express()
 
 const filterImages = e => makeImageObject(e.previewURL, e.webformatURL, e.tags, e.pageURL)
 const makeImageObject = (thumbnailUrl, webUrl, alt, pictureLocation) => {return new Object({thumbnailUrl, webUrl, alt, pictureLocation})}//I know I can use {{}} but I get weird bugs when I do, so it's staying like this
 
 const updateStorage = (query, arr) => {
-    SEARCH_STORE[query] = arr
+  
+    if(SEARCH_STORE.hasOwnProperty(query)){
+        delete SEARCH_STORE[query]
+    }
+    
+    let newData = {}
+    newData[query] = arr
+    
+    const newStore = Object.assign(newData, SEARCH_STORE)
+    SEARCH_STORE = newStore
     fs.writeFile(storeURL, JSON.stringify(SEARCH_STORE), 'utf-8')
     return arr
 }
@@ -22,6 +31,7 @@ const updateStorage = (query, arr) => {
 const getCachedResults = (query, offset) => {
     const cachedData = SEARCH_STORE[query]
     if(!cachedData) return false
+    updateStorage(query, cachedData)
     return paginate(cachedData, offset)
 }
 
@@ -39,6 +49,7 @@ app.get('/api/:query/:offset', (req,res) => {
     const {query, offset} = req.params
     const cachedData= getCachedResults(query, offset * 10)
     
+    
     if(cachedData){
         res.send(cachedData)
     } else {
@@ -46,9 +57,15 @@ app.get('/api/:query/:offset', (req,res) => {
         .then(r => r.hits.map(filterImages))
         .then(imageArray => updateStorage(query, imageArray))
         .then(imageArray => res.send(paginate(imageArray, offset * 10)))
+        .catch(err => console.log(err))
     }
 })
 
+app.get('/recent/', (req, res) => {
+   const keys = Object.keys(SEARCH_STORE)
+   
+   keys.length <= 10 ? res.send(keys) : res.send(keys.splice(0, 10))
+})
 
 app.listen(PORT, () => {
     console.log('Listening on :', PORT)
